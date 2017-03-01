b06bf5efac10e7b8a7088d5e5bf0f2c696b54bc8
Jacob Fisher <Jacob Fisher>
2017-02-28 19:40:22 -0500

    JUST REMEMBER TO CHECK THE frameRate thing

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
-function preload(){
+var cabin;
+var ThirstyMan;
+var DyingMan;
+var object;
+var myFont
+
+function preload() {
+    cabin = loadAnimation("CI/background_1.png", "CI/background_2.png");
+    ThirstyMan = loadAnimation("CI/ThirstyMan-1.png", "CI/ThirstyMan-2.png");
+    DyingMan = loadAnimation("CI/DyingMan_1.png", "CI/DyingMan_2.png");
+    object = loadAnimation("CI/object-1.png");
+    myFont = loadFont("CI/coders_crux.ttf");
 }
 
 function setup() {
-    createCanvas(800,800);
+    createCanvas(800, 800);
 
 }
 
 function draw() {
-    background(255,255,255);  
+    background(255, 0, 0);
+    animation(cabin, 400, 400);
+    animation(ThirstyMan, 250, 500);
+    animation(DyingMan, 500, 500);
+    animation(object, 140, 580);
+    frameRate(10);
 }
